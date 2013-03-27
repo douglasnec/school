80d43491281b8f3501c9b220e14bd861fb7c627b
douglas <douglas@douglas-laptop.(none)>
2013-03-27 17:11:38 -0300

    ajuste 1

--- db/migrate/20130305195159_create_people.rb
+++ db/migrate/20130305195159_create_people.rb
@@ -1,8 +1,8 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-      t.string name,
-      t.datetime birth,
+      t.string "name",
+      t.date "birth",
       t.timestamps
     end
   end
