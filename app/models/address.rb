3a202064653ca7c655cd678fa91764a98d3e401f
Douglas Nery <douglasnec@hotmail.com>
2013-04-01 17:57:56 -0300

    ajuste

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,9 +1,7 @@
 class Address < ActiveRecord::Base
   attr_accessible :adjunct, :city_id, :district, :number, :street, :type_address, :student_id, :interprise_id
   
+  validates_presence_of :street, :city_id
   belongs_to :student
-  belongs_to :interprise 
-  
-  
-  
+  belongs_to :interprise   
 end
